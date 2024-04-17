8aebd9e716a6b038cc910f2796f53b69b6c5a1fe
anandita01 <anandita0406@gmail.com>
2024-04-17 23:45:13 +0530

    .

---
+++ lost.js
@@ -0,0 +1,23 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Function to open the modal
+function openModal() {
+    modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+    modal.style.display = "none";
+}
+
+// Function to handle logout
+function logout() {
+    // Add your logout logic here
+   
+
+}
+
